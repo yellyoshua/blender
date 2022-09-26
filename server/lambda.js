1fafb7e4ec2c996ac01bf2881a16b38e2a7aff51
yellyoshua <usuario1529yoshua@hotmail.com>
2022-09-25 23:43:33 -0500

    add lambda with cache

--- server/lambda.js
+++ server/lambda.js
@@ -1,4 +1,19 @@
 const app = require('./dist/app.js').default;
+const db_connect = require('./dist/app.js').db_connect;
 const serverlessExpressHandler = require('@vendia/serverless-express');
 
-exports.handler = serverlessExpressHandler({app});
+let serverlessExpressInstance = null;
+
+async function setup (event, context) {
+  await db_connect();
+  serverlessExpressInstance = serverlessExpressHandler({app});
+  return serverlessExpressInstance(event, context);
+}
+
+exports.handler = (event, context) => {
+  if (serverlessExpressInstance) {
+    return serverlessExpressInstance(event, context);
+  }
+
+  return setup(event, context);
+};
