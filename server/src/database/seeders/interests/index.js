1c58abc26337b731fff9787f1d589ce0bb09958b
Mpqg <mapazquiga@gmail.com>
2022-09-27 12:43:27 -0500

    BL-3 - Interests/index

--- server/src/database/seeders/interests/index.js
+++ server/src/database/seeders/interests/index.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
-import music_interests, { INTERESTS_REF as music_ref } from './music_interests';
-import sport_interests, { INTERESTS_REF as sport_ref } from './sport_interests';
-import movie_interests, { INTERESTS_REF as movie_ref } from './movie_interests';
-import book_interests, { INTERESTS_REF as book_ref } from './book_interests';
-import cuisine_interests, { INTERESTS_REF as cuisine_ref } from './cuisine_interests';
-import beverage_interests, { INTERESTS_REF as beverage_ref } from './beverage_interests';
-import hobby_interests, { INTERESTS_REF as hobby_ref } from './hobby_interests'
-import religion_interests, { INTERESTS_REF as religion_ref } from './religion_interests';
-
+import music_interests, {INTERESTS_REF as music_ref} from './music_interests';
+import sport_interests, {INTERESTS_REF as sport_ref} from './sport_interests';
+import movie_interests, {INTERESTS_REF as movie_ref} from './movie_interests';
+import book_interests, {INTERESTS_REF as book_ref} from './book_interests';
+import cuisine_interests, {INTERESTS_REF as cuisine_ref} from './cuisine_interests';
+import beverage_interests, {INTERESTS_REF as beverage_ref} from './beverage_interests';
+import hobby_interests, {INTERESTS_REF as hobby_ref} from './hobby_interests';
+import religion_interests, {INTERESTS_REF as religion_ref} from './religion_interests';
+import political_interests, {INTERESTS_REF as political_ref} from './political_interests';
 
 const id = mongoose.Types.ObjectId;
 
@@ -19,13 +19,10 @@ export const INTERESTS_REF = {
   ...cuisine_ref,
   ...beverage_ref,
   ...hobby_ref,
-  ...religion_ref
-
-  political: id('623fb1b764fd56550e229ee0')
+  ...religion_ref,
+  ...political_ref
 };
-const food_genre = [
 
-];
 export default [
   ...music_interests,
   ...sport_interests,
@@ -35,4 +32,5 @@ export default [
   ...beverage_interests,
   ...hobby_interests,
   ...religion_interests,
+  ...political_interests
 ];
