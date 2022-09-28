c6b92063cdf6df07d42555cf71fe07aca4065144
yellyoshua <usuario1529yoshua@hotmail.com>
2022-09-28 12:33:20 -0500

    add models

--- server/src/modules/models.js
+++ server/src/modules/models.js
@@ -1,9 +1,13 @@
 import users from './users/users.model';
 import profiles from './profiles/profiles.model';
 import interests from './profiles/interests.model';
+import personalities from './profiles/personalities.models';
+import prompts from './prompts/prompts.model';
 
 export default {
   users,
   profiles,
-  interests
+  interests,
+  personalities,
+  prompts
 };
