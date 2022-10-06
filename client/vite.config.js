8ad138ec23e9a9ab12c98b289bfb9bd40530cc1a
yellyoshua <usuario1529yoshua@hotmail.com>
2022-10-06 16:08:10 -0500

    add support for PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    // eslint-disable-next-line new-cap
+    VitePWA({
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Blender',
+        short_name: 'Blender',
+        description: 'Blending cultures',
+        theme_color: '#473D5F',
+        icons: []
+      },
+      devOptions: {
+        enabled: false
+      }
+    })
+  ]
+});
