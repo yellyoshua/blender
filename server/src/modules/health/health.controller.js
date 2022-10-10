6a0c77af94003505db08f375d3e114739859ec40
yellyoshua <usuario1529yoshua@hotmail.com>
2022-10-10 14:15:47 -0500

    add node_env & page name

--- server/src/modules/health/health.controller.js
+++ server/src/modules/health/health.controller.js
@@ -2,6 +2,7 @@ import vars from '../../config/vars';
 
 export default {
   getHealth: () => {
+    console.log('vars.env :', vars.env);
     return {
       status: 'OK',
       timestamp: new Date().toISOString(),
