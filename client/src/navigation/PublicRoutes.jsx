90dce46e50c48c82a200c0803900d6ca684d38ec
Mpqg <mapazquiga@gmail.com>
2022-10-12 16:52:46 -0500

    BL-20-404 error page

--- client/src/navigation/PublicRoutes.jsx
+++ client/src/navigation/PublicRoutes.jsx
@@ -1,5 +1,6 @@
 import Login from '../modules/login';
 import Terms from '../modules/terms';
+import PageNotFound from '../modules/errors/pagenotfound';
 
 /**
  * @type {import('react-router').RouteObject[]}
@@ -12,5 +13,9 @@ export default [
   {
     path: '/terms-page',
     element: <Terms />
+  },
+  {
+    path: '/not-found',
+    element: <PageNotFound />
   }
 ];
