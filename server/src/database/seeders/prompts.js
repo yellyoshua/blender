5709b8ea7fbb9f3a13bdb23ff468064b80a7198d
Mpqg <mapazquiga@gmail.com>
2022-10-14 00:37:00 -0500

    BL-27 Prompt seeder

--- server/src/database/seeders/prompts.js
+++ server/src/database/seeders/prompts.js
@@ -1,18 +1,85 @@
-import {generateObjectId} from '../utils';
+import { generateObjectId } from '../utils';
 
 export const PROMPTS_REF = {
   prompts_1: generateObjectId('prompt_1'),
-  prompts_2: generateObjectId('prompt_2')
+  prompts_2: generateObjectId('prompt_2'),
+  prompts_3: generateObjectId('prompt_3'),
+  prompts_4: generateObjectId('prompt_4'),
+  prompts_5: generateObjectId('prompt_5'),
+  prompts_6: generateObjectId('prompt_6'),
+  prompts_7: generateObjectId('prompt_7'),
+  prompts_8: generateObjectId('prompt_8'),
+  prompts_9: generateObjectId('prompt_9'),
+  prompts_10: generateObjectId('prompt_10'),
+  prompts_11: generateObjectId('prompt_11'),
+  prompts_12: generateObjectId('prompt_12'),
+  prompts_13: generateObjectId('prompt_13'),
+  prompts_14: generateObjectId('prompt_14'),
+  prompts_15: generateObjectId('prompt_15'),
   
+
+
 };
 
 export default [
   {
     _id: PROMPTS_REF.prompts_1,
-    prompt: 'What is your favorite color?'
+    prompt: 'If I knew I wouldn't get judged, I...'
   },
   {
     _id: PROMPTS_REF.prompts_2,
-    prompt: 'What is your favorite food?'
+    prompt: 'The sense of life to me is...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_3,
+    prompt: 'I shall go on an adventure...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_4,
+    prompt: 'I could be the color...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_5,
+    prompt: 'If I could choose one characteristic of the people in my life, it would probably be...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_6,
+    prompt: 'If I had to choose between day and night, I would likely choose...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_7,
+    prompt: 'If I could choose one thing to do for the rest of my life, it would be...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_8,
+    prompt: 'If I had to quarantine with someone, I would choose...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_9,
+    prompt: 'Among large or small windows, I prefer...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_10,
+    prompt: 'Between plants or animals, I like...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_11,
+    prompt: 'The greatest fear I have is...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_12,
+    prompt: 'My work philosophy is...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_13,
+    prompt: 'I would like to be remembered as...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_14,
+    prompt: 'I look at myself as a successful person because...'
+  },
+  {
+    _id: PROMPTS_REF.prompts_15,
+    prompt: 'If I were an animal, I might be...'
   }
 ];
