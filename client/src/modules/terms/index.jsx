5871263ab271a0cb3f60bfcb885089dcaf38cac8
0806socrrn <socrrn@gmail.com>
2022-10-14 22:39:30 -0500

    add terms page

--- client/src/modules/terms/index.jsx
+++ client/src/modules/terms/index.jsx
@@ -4,73 +4,118 @@ export default function Terms () {
   return (
     <div className="">
       <div className="tittle">
-        <h1>
-          Privacy Policy
+        <h1 className="font-roboto">
+          Terms and Conditions
         </h1>
       </div>
       <div className="max-w-screen-xl m-auto">
         <div className="parrafo">
           <p>
-            This Privacy Policy sets out the terms on which Weblend uses and protects
-            information that is provided by its users
-            at the time of using its website.
-            This company is committed to the security of its users&apos; data.
-            When we ask you to fill in the fields of personal
-            information with which you can be identified,
-            we do so by ensuring that it will only be used
-            in accordance with the terms of this document.
-            However this Privacy Policy may change over time or
-            be updated so we encourage and emphasize you to
-            continually review this page to ensure that you agree to such changes.</p>
+            This Terms of Use Policy is valid as of Oct 2022.
+            Weblend, a legal entity under private law, describes, through this document, the rules of use of the https://weblend.app/ site and any other website, shop or application operated by the owner.
+
+            By browsing this website, we believe you agree to the terms of use below.
+
+            If you do not agree with the terms of this agreement,
+            we ask you not to make any other use of this website,
+            much less to register or submit your personal data.
+
+            If we change our terms of use, we will post the new text on this website,
+            with an updated review date. We may modify this document at any time.
+
+            If there is a significant change in the terms of this agreement,
+            we may inform you by using the contact information we have in our database
+            or by means of a notification.
+
+            Your use of this website after the changes means
+            that you accept the revised terms of use.
+            If, after reading the revised version,
+            you do not agree with its terms, please terminate your access.</p>
         </div>
         <h2 className="subtitulo">
-          Information that is collected
+          Section 1 - User
         </h2>
         <p className="parrafo">
-          Our website may collect personal information such as: Name,
-          contact information such as your email address
-          and demographic information. Also when necessary specific information may
-          be required to process an order or make a delivery or billing.</p>
+          The use of this website automatically grants you the status
+          of User and implies your full acceptance of all the guidelines and
+          conditions included in these Conditions.</p>
         <h2 className="subtitulo">
-          Use of the information collected
+          Section 2 - Adherence along with Privacy Policy
         </h2>
         <p className="parrafo">
-          Our website uses the information in order to provide the best possible service,
-          particularly to keep a record of users,
-          of orders if applicable, and to improve our products and services.
-          Emails may be sent periodically through our site with special offers,
-          new products and other advertising information that we consider
-          relevant to you or that may provide you with some benefit,
-          these emails will be sent to the address you provide and may be cancelled at any time.
-          Weblend is highly committed to keeping your information secure.
-          We use the most advanced systems and
-          constantly update them to ensure that there is no unauthorized access.</p>
+          Use of this website implies adherence to these Terms of Use
+          and the most up-to-date version of the Weblend Privacy Policy.</p>
         <h2 className="subtitulo">
-          Links to Third Parties
+          Section 3 - Conditions of access
         </h2>
         <p className="parrafo">
-          This website may contain links to other sites that may be of
-          interest to you. Once you click on these links and leave our site,
-          we no longer have control over the site to which you are redirected
-          and are therefore not responsible
-          for the terms or privacy or the protection of your data on those other third sites.
-          Such sites are subject to their own privacy policies so it is advisable
-          to consult them to confirm that you agree with them.</p>
+          In general, access to the Weblend website is free and does not require prior registration.
+          However, to make use of some features, the user may need to register,
+          creating a user account with a username and an access password.
+          It is the user is responsibility to provide only correct, authentic,
+          valid, complete and updated information, as well as not to disclose their
+          username and password to third parties.
+          Some parts of this website offer the user the option to post
+          comments in certain areas.Weblend does
+          not consent to the publication of content of a discriminatory, offensive or illegal
+          nature, or that infringe the copyright or any other rights of third parties.
+          The publication of any content by the user of this website, including messages
+          and comments, implys a non-exclusive, irrevocable and irreversible license for its use,
+          reproduction and publication by Weblend on its website, Internet platforms and
+          applications,
+          or even on other platforms, without any restrictions or limitations.</p>
         <h2 className="subtitulo">
-          Control of your personal information
+          Section 4 - Intellectual property
         </h2>
         <p className="parrafo">
-          At any time you may restrict the collection or use of personal
-          information that is provided to our website. Each time you are asked to fill out a form,
-          such as the user registration form, you can check or
-          uncheck the option to receive information
-          by email.In case you have checked the option to receive our
-          newsletter or advertising you can cancel it at any time.</p>
+          All elements of Weblend are the intellectual property of Weblend or its licensors.
+          These terms or the use of the website does not grant you any
+          license or right to use the intellectual property rights of
+          Weblend or any third party.</p>
+        <h2 className="subtitulo">
+          Section 6 - Links to third-party websites
+        </h2>
         <p className="parrafo">
-          This company will not sell, assign or distribute personal information that is
-          collected without your consent,
-          unless required by a judge with a court order.
-          Weblend reserves the right to change the terms of this Privacy Policy at any time.</p>
+          This website may contain, from time to time, hypertext links that will redirect you
+          to websites of our partners, advertisers, suppliers, etc. If you click on one of
+          these links to any of these sites,
+          Please remember that each site has its own privacy practices and that we are not
+          responsible
+          for these policies. Please refer to those policies before sending any personal data
+          to those sites.
+          We are not responsible for the collection, use and disclosure policies and practices
+          (including data protection practices) of other organizations, such as Facebook,
+          Apple, Google, Microsoft,
+          or any other software developer or application provider, social media store,
+          operating system,
+          internet service provider or device manufacturer,
+          including Personal Data that you disclose to other organizations through the applications,
+          in connection with such applications,
+          or published on our social networks.
+          We recommend that you be informed about the privacy policy and terms of use of
+          each website visited or service provider used.</p>
+        <h2 className="subtitulo">
+          Section 7 - Time limits and amendments
+        </h2>
+        <p className="parrafo">
+          The operation of this website is indefinite.
+          The entire website or each of its sections may be closed,
+          suspended or interrupted unilaterally by Weblend, at any
+          time and without prior notice.</p>
+        <h2 className="subtitulo">
+          Section 8 - Personal data
+        </h2>
+        <p className="parrafo">
+          During the use of this website, certain personal data will be
+          collected and processed by Weblend and/or the Partners.
+          The rules regarding the processing of Weblend is personal data
+          are stipulated in the Privacy Policy.</p>
+        <h2 className="subtitulo">
+          Section 9 - Contact
+        </h2>
+        <p className="parrafo">
+          If you have any questions about the terms of use,
+          please contact us by email mapazquiga@gmail.com</p>
       </div>
     </div>
   );
