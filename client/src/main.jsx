9fc25b4e6e7cf5175e0304a0063f37765ccb4348
Mpqg <mapazquiga@gmail.com>
2022-10-15 20:32:10 -0500

    BL-31 Birthday page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,16 @@ import Navigation from './navigation';
 import ResizeDevice from './modules/shared/components/ResizeDevice';
 import PWA from './modules/shared/components/PWA';
 import './global.css';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PWA />
-    <ResizeDevice>
-      <Navigation />
-    </ResizeDevice>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <PWA />
+      <ResizeDevice>
+        <Navigation />
+      </ResizeDevice>
+    </LocalizationProvider>
   </React.StrictMode>
 );
