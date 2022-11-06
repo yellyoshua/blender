2989b7472886f032fe0ffc397457c9918e170d3b
yellyoshua <usuario1529yoshua@hotmail.com>
2022-11-06 00:20:33 -0500

    add clarity script

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,23 @@ import Navigation from './navigation';
 import ResizeDevice from './modules/shared/components/ResizeDevice';
 import PWA from './modules/shared/components/PWA';
 import './global.css';
+import Script from './modules/shared/components/Script';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-
-deleteCache();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Script
+        env="prod"
+        content={`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "eek1ur8f52");
+        `}
+      />
       <PWA />
       <ResizeDevice>
         <Navigation />
@@ -19,15 +28,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </LocalizationProvider>
   </React.StrictMode>
 );
-
-// Delete browser cache to see the new version of the app
-
-function deleteCache () {
-  if (window.caches && window.caches.keys) {
-    caches.keys().then((names) => {
-      for (const name of names) {
-        caches.delete(name);
-      }
-    });
-  }
-}
