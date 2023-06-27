623160fba2b0dd961b39feea0c204983f2279e02
yellyoshua <usuario1529yoshua@hotmail.com>
2023-06-26 23:24:17 -0500

    Remove unnessesary method

--- app/lib/services/auth_service.dart
+++ app/lib/services/auth_service.dart
@@ -3,12 +3,4 @@ import 'package:app/main.dart';
 
 class WeblendAuthGoogleService extends CrudModel {
   WeblendAuthGoogleService() : super(WEBLEND_API_URL, 'auth/mobile/google');
-
-  Future<String> login(String accessToken) async {
-    final response = await post({
-      'accessToken': accessToken,
-    });
-
-    return response;
-  }
 }
