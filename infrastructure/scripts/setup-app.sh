b9c07d435d999bb5acf73483f35cdd56c33b5141
yellyoshua <usuario1529yoshua@hotmail.com>
2023-06-30 22:47:03 -0500

    Add app ci setup

---
+++ infrastructure/scripts/setup-app.sh
@@ -0,0 +1,8 @@
+
+echo "Setting up app..."
+CURRENT_FILE_PATH=$(dirname "$0")
+echo "base64 $RELEASE_KEYSTORE" | base64 -d > $CURRENT_FILE_PATH/../../app/android/app/weblend-release.keystore
+echo "base64 $RELEASE_KEYSTORE_PROPERTIES" | base64 -d > $CURRENT_FILE_PATH/../../app/android/release-keystore.properties
+
+
+
