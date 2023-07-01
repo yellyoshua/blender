91e3a420e89f93bc8398e420a9a9722f75565815
yellyoshua <usuario1529yoshua@hotmail.com>
2023-06-30 22:52:34 -0500

    Fix app ci path

--- infrastructure/scripts/setup-app.sh
+++ infrastructure/scripts/setup-app.sh
@@ -2,7 +2,6 @@
 echo "Setting up app..."
 CURRENT_FILE_PATH=$(dirname "$0")
 echo "base64 $RELEASE_KEYSTORE" | base64 -d > $CURRENT_FILE_PATH/../../app/android/app/weblend-release.keystore
-echo "base64 $RELEASE_KEYSTORE_PROPERTIES" | base64 -d > $CURRENT_FILE_PATH/../../app/android/release-keystore.properties
-
+echo "base64 $RELEASE_KEYSTORE_PROPERTIES" | base64 -d > $CURRENT_FILE_PATH/../../app/android/app/release-keystore.properties
 
 
