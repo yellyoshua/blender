94f926f9f3ca60c29d6be4441b23ec8777ce298a
yellyoshua <usuario1529yoshua@hotmail.com>
2023-06-30 22:58:52 -0500

    Fix app ci decode files

--- infrastructure/scripts/setup-app.sh
+++ infrastructure/scripts/setup-app.sh
@@ -1,7 +1,7 @@
 
 echo "Setting up app..."
 CURRENT_FILE_PATH=$(dirname "$0")
-echo "base64 $RELEASE_KEYSTORE" | base64 -d > $CURRENT_FILE_PATH/../../app/android/app/weblend-release.keystore
-echo "base64 $RELEASE_KEYSTORE_PROPERTIES" | base64 -d > $CURRENT_FILE_PATH/../../app/android/app/release-keystore.properties
+echo "$RELEASE_KEYSTORE" | base64 -d > $CURRENT_FILE_PATH/../../app/android/app/weblend-release.keystore
+echo "$RELEASE_KEYSTORE_PROPERTIES" | base64 -d > $CURRENT_FILE_PATH/../../app/android/app/release-keystore.properties
 
 
